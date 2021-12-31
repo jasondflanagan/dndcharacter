89d64b82ddfb2690b12f5dde5199baa680876a68
Jason Flanagan <jasondflanagan1@outlook.com>
2021-12-31 17:44:05 -0500

    RacesPage displaying properly. Needs CSS

--- src/components/RacesPageComponent.js
+++ src/components/RacesPageComponent.js
@@ -1,27 +1,37 @@
 import React, { Component } from 'react';
-import { Card, CardHeader, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, Tab, Tabs, Image } from 'react-bootstrap';
 import { RACES } from './RaceList';
 
 class RacesPage extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             races: RACES
         }
     }
+
     render() {
         const raceslist = this.state.races.map(race => {
             return (
-                <Card key={race.id}>
-                    <CardHeader>{race.name}</CardHeader>
-                    <img src={race.image} alt={race.name} />
+                <Card key={race.id} className="col-sm-4">
+                    <Tabs defaultActiveKey="image" id="race-tabs">
+                        <Tab eventKey="image" title={race.name}>
+                            <Image fluid src={race.image} alt={race.name}/>
+                        </Tab>
+                        <Tab eventKey="facts" title="Facts">
+                            {race.main}
+                        </Tab>
+                    </Tabs>
                 </Card>
             );
         });
 
         return (
             <div className="container">
+                <div className='row mt-3 mb-3'>
+                    <h1>Explore Available Races</h1>
+                </div>
                 <div className="row">
                     {raceslist}
                 </div>
