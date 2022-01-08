e3b6c60ac7d889b95813220b6d15c5d357070834
Jason Flanagan <jasondflanagan1@outlook.com>
2022-01-07 22:36:33 -0500

    Updated Styles

--- src/components/ClassesComponent.js
+++ src/components/ClassesComponent.js
@@ -13,14 +13,20 @@ class ClassesPage extends Component {
     render() {
         const classeslist = this.state.classes.map(classtype => {
             return (
-                <Card key={classtype.id} className="col-sm-4">
+                <Card key={classtype.id} className="col-sm-5 m-3">
                     <Card.Title>{classtype.name}</Card.Title>
-                    <Tabs defaultActiveKey="image" id="race-tabs">
+                    <Tabs defaultActiveKey="image">
                         <Tab eventKey="image" title='Image'>
-                            <Image fluid src={classtype.image} alt={classtype.name}/>
+                            <Image fluid src={classtype.imagetab} alt={classtype.name}/>
                         </Tab>
-                        <Tab eventKey="tab1title" title="text">
-                            {classtype.main}
+                        <Tab eventKey="tab1title" title={classtype.tab1title}>
+                            {classtype.tab1text}
+                        </Tab>
+                        <Tab eventKey="tab2title" title={classtype.tab2title}>
+                            {classtype.tab2text}
+                        </Tab>
+                        <Tab eventKey="tab3title" title={classtype.tab3title}>
+                            {classtype.tab3text}
                         </Tab>
                     </Tabs>
                 </Card>
@@ -28,9 +34,9 @@ class ClassesPage extends Component {
         });
 
         return (
-            <div className="container">
-                <div className='row pagetitle'>
-                    <p>Explore Available Races</p>
+            <div className="container" style={{backgroundImage: 'url(assets/parchment2.jpg)', marginLeft:0, marginRight:0, paddingRight: 0,display: 'inline-block', width: 'fit-content', backgroundSize: 'contain', backgroundRepeat: 'repeat'}}>
+            <div className='row pagetitle'>
+                    <p>Explore Available Classes</p>
                 </div>
                 <div className="row">
                     {classeslist}
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -23,7 +23,7 @@ class Header extends Component {
                     <div className="container">
                         <div className="row">
                                 <Collapse isOpen={this.state.isNavOpen} navbar>
-                                    <Nav navbar>
+                                    <Nav navbar style={{align: 'center', marginLeft: '7em'}}>
                                         <NavItem >
                                             <NavLink className="nav-link" style={{fontSize: "1.4em", color: "#eeec91"}} to="/home">
                                                 <i className="fa fa-university fa-lg" /> Home
