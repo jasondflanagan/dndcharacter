937df151458f525c5bceacbd5a71c14cccf40b59
Jason Flanagan <jasondflanagan1@outlook.com>
2022-01-07 23:06:47 -0500

    HomePage

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -9,9 +9,9 @@ class Home extends Component {
     }
     render() {
         return (
-          <React.Fragment>
-            <h1>home</h1>
-          </React.Fragment>
+          <div className='container' style={{backgroundImage: 'url(assets/TavernArt.jpg)', height:'100vh', backgroundSize:'cover', backgroundRepeat: 'no-repeat'}}>
+            <h1 style={{textAlign:'center', color: "#eeec91"}}>Welcome To The DND Tavern</h1>
+          </div>
         );
       }
     };
