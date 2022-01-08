4eefa6b354feedb6aa6372c02ff7356e74f13270
Jason Flanagan <jasondflanagan1@outlook.com>
2022-01-07 21:31:40 -0500

    ClassList Page is up, needs content

--- src/components/ClassesComponent.js
+++ src/components/ClassesComponent.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
+import { Card, Tab, Tabs, Image } from 'react-bootstrap';
+import { CLASSLIST } from '../shared/ClassesList';
 
 
 class ClassesPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            classes: CLASSLIST
         };
     }
     render() {
+        const classeslist = this.state.classes.map(classtype => {
+            return (
+                <Card key={classtype.id} className="col-sm-4">
+                    <Card.Title>{classtype.name}</Card.Title>
+                    <Tabs defaultActiveKey="image" id="race-tabs">
+                        <Tab eventKey="image" title='Image'>
+                            <Image fluid src={classtype.image} alt={classtype.name}/>
+                        </Tab>
+                        <Tab eventKey="tab1title" title="text">
+                            {classtype.main}
+                        </Tab>
+                    </Tabs>
+                </Card>
+            );
+        });
+
         return (
-            <React.Fragment>
-                <h1>Classes</h1>
-            </React.Fragment>
+            <div className="container">
+                <div className='row pagetitle'>
+                    <p>Explore Available Races</p>
+                </div>
+                <div className="row">
+                    {classeslist}
+                </div>
+            </div>
         );
     }
-};
+}
 
 export default ClassesPage;
