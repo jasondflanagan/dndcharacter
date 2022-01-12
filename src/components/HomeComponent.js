7d09de1ec258d969f7085d1c2dd6abf552dc0b47
Scheibelr <scheibelr@gmail.com>
2022-01-12 17:29:18 -0500

    parallax homepage

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
 
 class Home extends Component {
@@ -9,10 +10,77 @@ class Home extends Component {
     }
     render() {
         return (
-          <div style={{ backgroundImage: 'url(assets/parchment2.jpg)', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
-          <div className='container' style={{backgroundImage: 'url(assets/TavernArt.jpg)', height:'100vh', backgroundSize:'cover', backgroundRepeat: 'no-repeat'}}>
-            <h1 style={{textAlign:'center', color: "#eeec91"}}>Welcome To The DND Tavern</h1>
-          </div>
+          <div>
+              <Parallax pages={4.5} style={{backgroundColor: '#000'}}>
+                <ParallaxLayer
+                offset={0}
+                speed={0.1}
+                >
+                  <img
+                  alt="tavern"
+                  src="assets/TavernArt.jpg"
+                  style={{ width: "100%"}}
+                  />
+                </ParallaxLayer>
+                <ParallaxLayer
+                offset={.7}
+                speed={-.1}
+                style={{height: "100px"}}
+                >
+                  <h1 style={{justifyContent: 'center', textAlign:'center', alignItems: 'center', color: "#eeec91"}}>Welcome To The DND Tavern</h1>
+                </ParallaxLayer>
+                <ParallaxLayer
+                offset={1.5}
+                speed={.2}
+                style={{height: "100px"}}
+                >
+                  <h1 style={{justifyContent: 'center', textAlign:'center', alignItems: 'center', color: "#eeec91"}}>Grab Your Equipment</h1>
+                </ParallaxLayer>
+                <ParallaxLayer
+                offset={1.9}
+                speed={.7}
+                >
+                <img
+                alt="dragons"
+                src="assets/DiceCharacterSheet.webp"
+                style={{ width: "100%"}}
+                />
+                </ParallaxLayer>
+                <ParallaxLayer
+                offset={2.6}
+                speed={.2}
+                style={{height: "100px"}}
+                >
+                  <h1 style={{justifyContent: 'center', textAlign:'center', alignItems: 'center', color: "#eeec91"}}>Gather Your Friends</h1>
+                </ParallaxLayer>
+                <ParallaxLayer
+                offset={2.9}
+                speed={.7}
+                >
+                <img
+                alt="dragons"
+                src="assets/PartyVsGreenDragon.jpg"
+                style={{ width: "100%"}}
+                />
+                </ParallaxLayer>
+                <ParallaxLayer
+                offset={3.5}
+                speed={.2}
+                style={{height: "100px"}}
+                >
+                  <h1 style={{justifyContent: 'center', textAlign:'center', alignItems: 'center', color: "#eeec91"}}>And Prepare For Battle!</h1>
+                </ParallaxLayer>
+                <ParallaxLayer
+                offset={3.9}
+                speed={.7}
+                >
+                <img
+                alt="dragons"
+                src="assets/Tiamat.jpg"
+                style={{ width: "100%"}}
+                />
+                </ParallaxLayer>
+              </Parallax>
           </div>
         );
       }
